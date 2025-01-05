15447b2f24f3e8ec138d9f8303be9680c140ca32
John Doe <queenarossa@gmail.com>
2025-01-05 17:44:01 +0700

    ubah judul form login 2

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login | Weklcome My Daily Journal</title>
+    <title>Login | Welcome My Daily Journal</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
